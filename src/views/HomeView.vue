837f44028c42bb6028bfcd299dec9984592897d3
thantharhnin17 <thanthark88@gmail.com>
2023-11-06 16:49:50 +0630

    Upgrade using CLI

---
+++ src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <ProductCard 
+          v-for="(product, index) in inventory.slice(0, 3)" 
+          :key="product.id" 
+          :product="product" 
+          :index="index"
+          :addToCart="addToCart"
+        />
+
+
+      </div>
+
+    </main>
+    
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  name: 'HomeView',
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,29 @@
+<template>
+    <main class="wrapper">
+        <h1>Products</h1>
+
+        <div class="card-container">
+
+          <ProductCard 
+          v-for="(product, index) in inventory" 
+          :key="product.id" 
+          :product="product" 
+          :index="index"
+          :addToCart="addToCart"
+        />
+
+        </div>
+      </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+
+</script>
